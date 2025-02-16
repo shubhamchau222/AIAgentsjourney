abfa31219668534cc7c7e9b1827562e79e591c2e
shubhamchau222 <shubhamchau78@gmail.com>
2025-02-16 23:17:07 +0530

    e2e rag utils updated

---
+++ RAG/E2E_RAG/api/langchain_utils.py
@@ -0,0 +1,41 @@
+from langchain_groq import ChatGroq
+from langchain_core.output_parsers import StrOutputParser, JsonOutputParser
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains import history_aware_retriever, create_retrieval_chain
+from typing import List
+from langchain_core.documents import Document
+from chroma_utils import vectore_store
+import os 
+
+retriever=vectore_store.as_retriever()
+
+output_parser= StrOutputParser()
+
+# Set up prompts and chains
+contextualize_q_system_prompt = (
+    "Given a chat history and the latest user question "
+    "which might reference context in the chat history, "
+    "formulate a standalone question which can be understood "
+    "without the chat history. Do NOT answer the question, "
+    "just reformulate it if needed and otherwise return it as is."
+)
+
+#contextualize prompt
+contextualize_prompt= ChatPromptTemplate.from_messages([
+                    ("system", contextualize_q_system_prompt),
+                    MessagesPlaceholder("chat_history"),
+                    ("human", "{input}")
+                ])
+
+qa_prompt = ChatPromptTemplate.from_messages([
+            ("system", "you are an helpful AI Assistant who always response respectfully. Use the following context to answer the user's question."),
+            ("system", "context: {context}"),
+            MessagesPlaceholder(variable_name="chat_history"),
+            ("human", "{input}")
+        ])
+
+def get_rag_chain(model="llama-3.1-8b-instant"):
+    llm= ChatGroq(model)
+    history_aware_retriever =history_aware_retriever(llm, retriever, contextualize_prompt)
+    pass
--- RAG/E2E_RAG/api/pydantic_models_format.py
+++ RAG/E2E_RAG/api/pydantic_models_format.py
@@ -1,27 +1,33 @@
-# This file will containt the data type restrictions classes
-# certain parameters or the arguments should follow mentioned datatype
+# This file will contain the data type restriction classes
+# Certain parameters or arguments should follow the mentioned datatype
+
 from pydantic import Field, BaseModel
 from enum import Enum
 from datetime import datetime
 
+# Enum class for model names
 class ModelName(str, Enum):
-    LLAMA_8BINSTANT= "llama-3.1-8b-instant"
+    LLAMA_8BINSTANT = "llama-3.1-8b-instant"
     GEMMA = "gemma2-9b-it"
 
+# Pydantic model for query input
 class QueryInput(BaseModel):
     question: str
     session_id: str = Field(default=None)
-    model: ModelName = Field(default= ModelName.LLAMA_8BINSTANT)
+    model: ModelName = Field(default=ModelName.LLAMA_8BINSTANT)
 
+# Pydantic model for query response
 class QueryResponse(BaseModel):
-    answer : str
-    session_id:str 
+    answer: str
+    session_id: str
     model: ModelName
 
+# Pydantic model for document information
 class DocumentInfo(BaseModel):
     id: int
     filename: str
     upload_timestamp: datetime
 
+# Pydantic model for delete file request
 class DeleteFileRequest(BaseModel):
-    file_id:int
+    file_id: int
